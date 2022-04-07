91a909462ce448dc350a62a3db858ea645e80779
Crisjra <98718225+Crisjra@users.noreply.github.com>
2022-04-06 21:37:19 -0600

    Add files via upload

---
+++ Ejercicio15 - Cuadro de notas/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream> 
+#include <math.h>
+
+uso de std de espacio de nombres ;
+
+int main()
+{
+	float nota = 0;
+	
+	cout<<" Ingrese la nota del estudiante: " << endl;
+	cin >> nota;
+	
+	if (nota > 100 || nota < 0) 
+	{
+	cout << " Posibilidades validas del 0 al 100 " << endl;
+	
+	}
+	más
+	if (nota == 100) 
+	{
+		cout << " Resultado recibido una A++ " << endl; 
+	}
+	más
+	if (nota >= 95 && nota <= 99)
+	{
+		cout << " Resultado recibido una A+ " << endl;
+	}
+	más
+	if (nota >= 90 && nota <= 94)
+	{
+		cout << " Resultado recibido una A " << endl;
+	}
+	más
+	if (nota >= 85 && nota <= 89)
+	{
+		cout << " Resultado recibido una A- " << endl;
+	}
+	más
+	if (nota >= 80 && nota <= 84)
+	{
+		cout << " Resultado recibido una B+ " << endl;
+	}
+	más
+	if (nota >= 75 && nota <= 79)
+	{
+		cout << " Resultado recibido una B " << endl;
+	}
+	más
+	if (nota >= 70 && nota <= 74)
+	{
+		cout << " Resultado recibido una B- " << endl;
+	}
+	más
+	if (nota >= 65 && nota <= 69)
+	{
+		cout << " Resultado recibido una C+ " << endl;
+	}
+	más
+	if (nota >= 60 && nota <= 64)
+	{
+		cout << " Resultado recibido una C " << endl;
+	}
+	más
+	if (nota >= 55 && nota <= 59)
+	{
+		cout << " Resultado recibido una C- " << endl;
+	}
+	más
+	if (nota >= 50 && nota <= 54)
+	{
+		cout << " Resultado recibido una D+ " << endl;
+	}
+	más
+	if (nota >= 45 && nota <= 49)
+	{
+		cout << " Resultado recibido una D " << endl;
+	}
+	else
+	if (nota >= 40 && nota <= 44)
+	{
+		cout << " Resultado recibido una D- " << endl;
+	}
+	else
+	if (nota >= 35 && nota <= 39)
+	{
+		cout << " Resultado recibido una E+ " << endl;
+	}
+	else
+	if (nota >= 30 && nota <= 34)
+	{
+		cout << " Resultado recibido una E " << endl;
+	}
+	más
+	if (nota >= 25 && nota <= 29)
+	{
+		cout << " Resultado recibido una E- " << endl;
+	}
+	más
+	if (nota >= 20 && nota <= 24)
+	{
+		cout << " Resultado recibido una F+ " << endl;
+	}
+	más
+	if (nota >= 15 && nota <= 19)
+	{
+		cout << " Resultado recibido una F " << endl;
+	}
+	más
+	if (nota >= 10 && nota <= 14)
+	{
+		cout << " Resultado recibido una F " << endl;
+	}
+	más
+	if (nota >= 5 && nota <= 9)
+	{
+		cout << " Resultado recibido una F- " << endl;
+	}
+	más
+	if (nota >= 0 && nota <= 4)
+	{
+		cout << " Resultado recibido una F-- " << endl;
+	}
+		
+	volver 0;
+}
